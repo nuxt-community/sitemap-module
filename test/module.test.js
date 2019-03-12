6744a6648b5bf3f9a4357c652a58ec38448a1d55
Nicolas PENNEC <NicoPennec@users.noreply.github.com>
2019-03-12 01:28:17 +0100

    fix: add child-routes to sitemap.xml (#49)

--- test/module.test.js
+++ test/module.test.js
@@ -24,31 +24,61 @@ describe('ssr', () => {
   })
 
   test('render', async () => {
+    // static routes
     let html = await get('/')
     expect(html).toContain('/index')
     html = await get('/sub/')
     expect(html).toContain('/sub/index')
     html = await get('/sub/sub')
     expect(html).toContain('/sub/sub')
+
+    // static child-routes
+    html = await get('/parent')
+    expect(html).toContain('/parent')
+    html = await get('/parent/child')
+    expect(html).toContain('/parent/child')
+    html = await get('/parent/child/subchild')
+    expect(html).toContain('/parent/child/subchild')
+
+    // dynamic routes
     html = await get('/child/')
     expect(html).toContain('/child/index')
     html = await get('/child/1')
     expect(html).toContain('/child/1')
     html = await get('/1/')
     expect(html).toContain('/1/index')
+
+    // excluded routes
     html = await get('/exclude')
     expect(html).toContain('/exclude')
+
+    // filtered routes
+    html = await get('/filtered')
+    expect(html).toContain('/filtered')
   })
 
   test('sitemap', async () => {
     const xml = await get('/sitemap.xml')
+
+    // static routes
     expect(xml).toContain('<loc>http://localhost:3000/</loc>')
     expect(xml).toContain('<loc>http://localhost:3000/sub</loc>')
     expect(xml).toContain('<loc>http://localhost:3000/sub/sub</loc>')
+
+    // static child-routes
+    expect(xml).toContain('<loc>http://localhost:3000/parent</loc>')
+    expect(xml).toContain('<loc>http://localhost:3000/parent/child</loc>')
+    expect(xml).toContain('<loc>http://localhost:3000/parent/child/subchild</loc>')
+
+    // dynamic routes
     expect(xml).toContain('<loc>http://localhost:3000/child</loc>')
     expect(xml).toContain('<loc>http://localhost:3000/child/1</loc>')
     expect(xml).toContain('<loc>http://localhost:3000/1/</loc>')
+
+    // excluded routes
     expect(xml).not.toContain('<loc>http://localhost:3000/exclude</loc>')
+
+    // filtered routes
     expect(xml).not.toContain('<loc>http://localhost:3000/filtered</loc>')
   })
 
