e78e4a26fff976d83120c2cfe02763aaba1ffc0e
NicoPennec <nemohp@yahoo.fr>
2019-09-24 01:14:15 +0200

    feat: add configuration for sitemap index and multiple sitemaps
    fix #6
    
    BREAKING CHANGE: Drop support for Nuxt.js 1.x

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -4,19 +4,36 @@ module.exports = {
     resourceHints: false
   },
   modules: [require('../..')],
-  sitemap: {
-    path: '/sitemap.xml',
-    exclude: ['/exclude'],
-    gzip: true,
-    hostname: 'http://localhost:3000/',
-    routes: ['1/', 'child/1', { url: 'test' }],
-    filter: ({ routes }) => routes.filter(route => route.url !== '/filtered'),
-    xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
-    // xslUrl: 'sitemap.xsl',
-    trailingSlash: true,
-    defaults: {
-      changefreq: 'daily',
-      priority: 1
+  sitemap: [
+    {
+      path: '/sitemap.xml',
+      exclude: ['/exclude'],
+      gzip: true,
+      hostname: 'http://localhost:3000/',
+      routes: ['1/', 'child/1', { url: 'test' }],
+      filter: ({ routes }) => routes.filter(route => route.url !== '/filtered/'),
+      xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
+      // xslUrl: 'sitemap.xsl',
+      trailingSlash: true,
+      defaults: {
+        changefreq: 'daily',
+        priority: 1
+      }
+    },
+    {
+      hostname: 'https://example.com/',
+      lastmod: new Date().toISOString(),
+      sitemaps: [
+        {
+          path: '/sitemap-foo.xml',
+          routes: ['foo/1', 'foo/2']
+        },
+        {
+          hostname: 'https://yolo.com/',
+          path: '/sitemap-bar.xml',
+          routes: ['bar/1', 'bar/2']
+        }
+      ]
     }
-  }
+  ]
 }
