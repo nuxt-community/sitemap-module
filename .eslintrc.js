a95531817ad33911d1215716d280d59a6d7751b9
NicoPennec <nemohp@yahoo.fr>
2020-04-05 23:41:15 +0200

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
 module.exports = {
-  extends: [
-    '@nuxtjs',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['@nuxtjs', 'plugin:prettier/recommended'],
   rules: {
+    curly: 'error',
     // Allow sparse arrays
-    'no-sparse-arrays': 'off'
-  }
+    'no-sparse-arrays': 'off',
+  },
 }
