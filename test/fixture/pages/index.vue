2b8f3b05eee5fdb68d350e28a89f6d6dcada37a6
NicoPennec <nemohp@yahoo.fr>
2020-12-06 00:03:30 +0100

    chore: fix formating

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
     /index
-    <nuxt-link to="/crawler-1">
-      /crawler-1
-    </nuxt-link>
-    <nuxt-link to="/child/crawler-2">
-      /child/crawler-2
-    </nuxt-link>
+    <nuxt-link to="/crawler-1">/crawler-1</nuxt-link>
+    <nuxt-link to="/child/crawler-2">/child/crawler-2</nuxt-link>
   </div>
 </template>
