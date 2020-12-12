afc8800ec23991b9add687aaa80285571213cd51
Jimmy Jouanne <jimmy.jouanne@gmail.com>
2020-12-12 12:05:46 +0100

    fix: nuxt config js prettier

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -4,8 +4,8 @@ export default theme({
   i18n: {
     locales: () => [
       { code: 'fr', iso: 'fr-FR', file: 'fr-FR.js', name: 'Français' },
-      { code: 'en', iso: 'en-US', file: 'en-US.js', name: 'English' }
+      { code: 'en', iso: 'en-US', file: 'en-US.js', name: 'English' },
     ],
-    defaultLocale: 'en'
-  }
+    defaultLocale: 'en',
+  },
 })
